a185b287ab168e7e3c99efe3a5cd7c9b39e29dfc
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-07-25 11:10:49 -0500

    Javadoc

--- src/main/java/io/justina/management/service/authentication/IAuthenticationService.java
+++ src/main/java/io/justina/management/service/authentication/IAuthenticationService.java
@@ -3,25 +3,16 @@ package io.justina.management.service.authentication;
 import io.justina.management.dto.jwttoken.DataJWTTokenDTO;
 import io.justina.management.dto.user.UserAuthenticateDataDTO;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * Interfaz que define métodos para la autenticación y gestión de usuarios en el sistema.
  */
 public interface IAuthenticationService {
 
-    /**
-     * Carga los detalles del usuario por su nombre de usuario (correo electrónico).
-     *
-     * @param username Nombre de usuario (correo electrónico) del usuario.
-     * @return Detalles del usuario encontrado.
-     * @throws UsernameNotFoundException Si el usuario no se encuentra en la base de datos.
-     */
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+    //UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 
     /**
-     * Autentica a un usuario con las credenciales proporcionadas y devuelve un token JWT.
+     * Autentíca a un usuario con las credenciales proporcionadas y devuelve un token JWT.
      *
      * @param userAuthenticateDataDTO Datos de autenticación del usuario (correo electrónico y contraseña).
      * @return DTO que contiene el token JWT generado.
